865b2fb6dacfdf62523c458bcf7fe87895c95479
unknown <you@example.com>
2021-12-26 15:54:21 -0500

    Login, Login Status, Logout, Signup API

--- twitter/urls.py
+++ twitter/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from accounts.api.views import UserViewSet
+from accounts.api import views
 
 router = routers.DefaultRouter()
-router.register(r'api/users', UserViewSet)
-
+router.register(r'api/users', views.UserViewSet)
+router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
