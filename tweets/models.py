ed8fe22dbff732940fdfe3e2cfe1e5bd7435128d
unknown <you@example.com>
2022-01-03 12:23:10 -0500

    Add liked model and admin

--- tweets/models.py
+++ tweets/models.py
@@ -1,5 +1,7 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from likes.models import Like
 from utils.time_helpers import utc_now
 
 
@@ -17,11 +19,18 @@ class Tweet(models.Model):
         index_together = (('user', 'created_at'),)
         ordering = ('user', '-created_at')
 
+    def __str__(self):
+        # Here is what will be displayed when you execute print(tweet instance)
+        return f'{self.created_at} {self.user}: {self.content}'
+
     @property
     def hours_to_now(self):
         # datetime.now without time zone information,  need to add utc time zone information
         return (utc_now() - self.created_at).seconds // 3600
 
-    def __str__(self):
-        # Here is the content that will be displayed when you execute print(tweet instance)
-        return f'{self.created_at} {self.user}: {self.content}'
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Tweet),
+            object_id=self.id,
+        ).order_by('-created_at')
