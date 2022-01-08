9e4e2c8aa23c43413bb50dc0fb42772271a7b47b
unknown <you@example.com>
2022-01-07 22:32:25 -0500

    add newfeeds pagination function

--- newsfeeds/api/tests.py
+++ newsfeeds/api/tests.py
@@ -1,7 +1,8 @@
-from newsfeeds.models import NewsFeed
 from friendships.models import Friendship
 from rest_framework.test import APIClient
 from testing.testcases import TestCase
+from newsfeeds.models import NewsFeed
+from utils.paginations import EndlessPagination
 
 
 NEWSFEEDS_URL = '/api/newsfeeds/'
@@ -29,26 +30,82 @@ class NewsFeedApiTests(TestCase):
             Friendship.objects.create(from_user=self.wl_hsu, to_user=following)
 
     def test_list(self):
-        # 需要登录
+        # Login required
         response = self.anonymous_client.get(NEWSFEEDS_URL)
         self.assertEqual(response.status_code, 403)
-        # 不能用 post
+        # Can't use post
         response = self.wl_client.post(NEWSFEEDS_URL)
         self.assertEqual(response.status_code, 405)
-        # 一开始啥都没有
+        # Nothing at first
         response = self.wl_client.get(NEWSFEEDS_URL)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data['newsfeeds']), 0)
-        # 自己发的信息是可以看到的
+        self.assertEqual(len(response.data['results']), 0)
+        # user can see his/her own messages
         self.wl_client.post(POST_TWEETS_URL, {'content': 'Hello World'})
         response = self.wl_client.get(NEWSFEEDS_URL)
-        self.assertEqual(len(response.data['newsfeeds']), 1)
-        # 关注之后可以看到别人发的
+        self.assertEqual(len(response.data['results']), 1)
+        # 关After following, user can see other people's posts
         self.wl_client.post(FOLLOW_URL.format(self.wl_hsu.id))
         response = self.wl_hsu_client.post(POST_TWEETS_URL, {
             'content': 'Hello Twitter',
         })
         posted_tweet_id = response.data['id']
         response = self.wl_client.get(NEWSFEEDS_URL)
-        self.assertEqual(len(response.data['newsfeeds']), 2)
-        self.assertEqual(response.data['newsfeeds'][0]['tweet']['id'], posted_tweet_id)
+        self.assertEqual(len(response.data['results']), 2)
+        self.assertEqual(response.data['results'][0]['tweet']['id'], posted_tweet_id)
+
+    def test_pagination(self):
+        page_size = EndlessPagination.page_size
+        followed_user = self.create_user('followed')
+        newsfeeds = []
+        for i in range(page_size * 2):
+            tweet = self.create_tweet(followed_user)
+            newsfeed = self.create_newsfeed(user=self.wl, tweet=tweet)
+            newsfeeds.append(newsfeed)
+
+        newsfeeds = newsfeeds[::-1]
+
+        # pull the first page
+        response = self.wl_client.get(NEWSFEEDS_URL)
+        self.assertEqual(response.data['has_next_page'], True)
+        self.assertEqual(len(response.data['results']), page_size)
+        self.assertEqual(response.data['results'][0]['id'], newsfeeds[0].id)
+        self.assertEqual(response.data['results'][1]['id'], newsfeeds[1].id)
+        self.assertEqual(
+            response.data['results'][page_size - 1]['id'],
+            newsfeeds[page_size - 1].id,
+        )
+
+        # pull the second page
+        response = self.wl_client.get(
+            NEWSFEEDS_URL,
+            {'created_at__lt': newsfeeds[page_size - 1].created_at},
+        )
+        self.assertEqual(response.data['has_next_page'], False)
+        results = response.data['results']
+        self.assertEqual(len(results), page_size)
+        self.assertEqual(results[0]['id'], newsfeeds[page_size].id)
+        self.assertEqual(results[1]['id'], newsfeeds[page_size + 1].id)
+        self.assertEqual(
+            results[page_size - 1]['id'],
+            newsfeeds[2 * page_size - 1].id,
+        )
+
+        # pull latest newsfeeds
+        response = self.wl_client.get(
+            NEWSFEEDS_URL,
+            {'created_at__gt': newsfeeds[0].created_at},
+        )
+        self.assertEqual(response.data['has_next_page'], False)
+        self.assertEqual(len(response.data['results']), 0)
+
+        tweet = self.create_tweet(followed_user)
+        new_newsfeed = self.create_newsfeed(user=self.wl, tweet=tweet)
+
+        response = self.wl_client.get(
+            NEWSFEEDS_URL,
+            {'created_at__gt': newsfeeds[0].created_at},
+        )
+        self.assertEqual(response.data['has_next_page'], False)
+        self.assertEqual(len(response.data['results']), 1)
+        self.assertEqual(response.data['results'][0]['id'], new_newsfeed.id)
