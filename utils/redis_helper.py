1916bb6721416f10a49c0130fcd630c58aefe613
unknown <you@example.com>
2022-01-09 18:04:00 -0500

    cache tweets list in redis

---
+++ utils/redis_helper.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+from utils.redis_client import RedisClient
+from utils.redis_serializers import DjangoModelSerializer
+
+
+class RedisHelper:
+
+    @classmethod
+    def _load_objects_to_cache(cls, key, objects):
+        conn = RedisClient.get_connection()
+
+        serialized_list = []
+        for obj in objects:
+            serialized_data = DjangoModelSerializer.serialize(obj)
+            serialized_list.append(serialized_data)
+
+        if serialized_list:
+            conn.rpush(key, *serialized_list)
+            conn.expire(key, settings.REDIS_KEY_EXPIRE_TIME)
+
+    @classmethod
+    def load_objects(cls, key, queryset):
+        conn = RedisClient.get_connection()
+
+        # If it exists in the cache, take it out directly and return
+        if conn.exists(key):
+            serialized_list = conn.lrange(key, 0, -1)
+            objects = []
+            for serialized_data in serialized_list:
+                deserialized_obj = DjangoModelSerializer.deserialize(serialized_data)
+                objects.append(deserialized_obj)
+            return objects
+
+        cls._load_objects_to_cache(key, queryset)
+
+        # The reason for converting to list is to keep the return type uniform,
+        # because the data stored in redis is in the form of list
+        return list(queryset)
+
+    @classmethod
+    def push_object(cls, key, obj, queryset):
+        conn = RedisClient.get_connection()
+        if not conn.exists(key):
+            # If the key does not exist, load directly from the database without adding a single push to the cache
+            cls._load_objects_to_cache(key, queryset)
+            return
+        serialized_data = DjangoModelSerializer.serialize(obj)
+        conn.lpush(key, serialized_data)
