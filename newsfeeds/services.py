a1b4c50be2cfd2bf69a869aed169db1852bc7880
unknown <you@example.com>
2022-01-09 19:52:56 -0500

    Cache newsfeeds in redis

--- newsfeeds/services.py
+++ newsfeeds/services.py
@@ -1,5 +1,8 @@
-from newsfeeds.models import NewsFeed
 from friendships.services import FriendshipService
+from newsfeeds.models import NewsFeed
+from newsfeeds.models import NewsFeed
+from twitter.cache import USER_NEWSFEEDS_PATTERN
+from utils.redis_helper import RedisHelper
 
 
 class NewsFeedService(object):
@@ -20,3 +23,18 @@ class NewsFeedService(object):
         ]
         newsfeeds.append(NewsFeed(user=tweet.user, tweet=tweet))
         NewsFeed.objects.bulk_create(newsfeeds)
+        # bulk create will not trigger the post_save signal, so you need to manually push it to the cache
+        for newsfeed in newsfeeds:
+            cls.push_newsfeed_to_cache(newsfeed)
+
+    @classmethod
+    def get_cached_newsfeeds(cls, user_id):
+        queryset = NewsFeed.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_newsfeed_to_cache(cls, newsfeed):
+        queryset = NewsFeed.objects.filter(user_id=newsfeed.user_id).order_by('-created_at')
+        key = USER_NEWSFEEDS_PATTERN.format(user_id=newsfeed.user_id)
+        RedisHelper.push_object(key, newsfeed, queryset)
